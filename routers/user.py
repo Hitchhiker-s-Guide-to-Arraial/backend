696e6befae5f6a10fb040627e9ac5b5b51bb1837
Alves <pedro.alves.ferreira@celfocus.com>
2025-10-17 10:38:36 +0100

    added jwt to login

--- routers/user.py
+++ routers/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from db.database import get_db
 from schemas.user import UserCreate, UserLogin
 from services.userService import create_user, authenticate_user
+import jwt
 
 router = APIRouter(tags=["User"])
 
@@ -16,4 +17,6 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = authenticate_user(user, db)
     if not db_user:
         raise HTTPException(status_code=400, detail="Invalid credentials")
-    return {"id": db_user.id, "name": db_user.name, "email": db_user.email}
+    token = jwt.encode({"sub": user.email}, "secret", algorithm="HS256")
+
+    return {"id": db_user.id, "name": db_user.name, "email": db_user.email, "token": token}
